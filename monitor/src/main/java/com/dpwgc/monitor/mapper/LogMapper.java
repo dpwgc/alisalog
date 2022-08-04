e547f995ab9c03906c994bebfb48023317f48e43
dpwgc <281148314@qq.com>
2022-08-04 23:06:40 +0800

    update

--- monitor/src/main/java/com/dpwgc/monitor/mapper/LogMapper.java
+++ monitor/src/main/java/com/dpwgc/monitor/mapper/LogMapper.java
@@ -2,6 +2,8 @@ package com.dpwgc.monitor.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dpwgc.monitor.model.response.Log;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface LogMapper extends BaseMapper<Log> {
 }
