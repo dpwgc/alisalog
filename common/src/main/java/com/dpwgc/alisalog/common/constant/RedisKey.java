bcd11a93ed941628c7557b67ac59f5f56190c2f6
dpwgc <281148314@qq.com>
2022-08-12 12:01:21 +0800

    引入redis

---
+++ common/src/main/java/com/dpwgc/alisalog/common/constant/RedisKey.java
@@ -0,0 +1,18 @@
+package com.dpwgc.alisalog.common.constant;
+
+public interface RedisKey {
+
+    String IDC_SET_KEY = "idc-set";
+
+    String HOST_SET_KEY = "host-set";
+
+    String ENV_SET_KEY = "env-set";
+
+    String APP_ID_SET_KEY = "appId-set";
+
+    String MODULE_SET_KEY = "module-set";
+
+    String CATEGORY_SET_KEY = "category-set";
+
+    String SUB_CATEGORY_SET_KEY = "sub-category-set";
+}
---
+++ worker/src/main/java/com/dpwgc/alisalog/worker/store/RedisClient.java
@@ -0,0 +1,113 @@
+package com.dpwgc.alisalog.worker.store;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import javax.annotation.Resource;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Redis工具类
+ */
+@Component
+public class RedisClient {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 用来判断key是否存在
+     */
+    public boolean hasKey(String key){
+        if(key==null){
+            return false;
+        }else{
+            return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+        }
+    }
+
+    /**
+     * 指定缓存失效时间
+     *
+     * @param key  键
+     * @param time 时间(秒)
+     * @return
+     */
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //============================set=============================
+
+    /**
+     * 根据key获取Set中的所有值
+     *
+     * @param key 键
+     * @return
+     */
+    public Set<Object> sGet(String key) {
+        try {
+            return redisTemplate.opsForSet().members(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 根据value从一个set中查询,是否存在
+     *
+     * @param key   键
+     * @param value 值
+     * @return true 存在 false不存在
+     */
+    public boolean sHasKey(String key, Object value) {
+        try {
+            return redisTemplate.opsForSet().isMember(key, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 将数据放入set缓存
+     *
+     * @param key    键
+     * @param values 值 可以是多个
+     * @return 成功个数
+     */
+    public long sSet(String key, Object... values) {
+        try {
+            return redisTemplate.opsForSet().add(key, values);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    /**
+     * 移除值为value的
+     *
+     * @param key    键
+     * @param values 值 可以是多个
+     * @return 移除的个数
+     */
+    public long setRemove(String key, Object... values) {
+        try {
+            Long count = redisTemplate.opsForSet().remove(key, values);
+            return count;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+}
